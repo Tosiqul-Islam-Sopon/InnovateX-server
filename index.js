31371e0ff0d887ded6278d9804829fe2c82f17ed
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-06-03 17:05:51 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to InnovateX!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
